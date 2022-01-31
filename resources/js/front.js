fea43780a42146ec51655fcd4b5dbe343c428daa
Bonu93 <luca.morucci.kb@gmail.com>
2022-01-31 15:21:21 +0100

    add vue

--- resources/js/front.js
+++ resources/js/front.js
@@ -3,7 +3,7 @@ window.Vue = require('vue');
 import Vue from 'vue';
 import App from './views/App.vue';
 
-const App = new Vue({
+const app = new Vue({
     el: '#root',
     render: h => h(App)
 })
