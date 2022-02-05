a7d7def9bc85515df444e8d6fbc1c08ce9ca5338
Bonu93 <luca.morucci.kb@gmail.com>
2022-02-05 13:32:41 +0100

    add tags in posts show view

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -6,6 +6,18 @@
         <h5>@if ($post->category) {{$post->category->name}} @else Uncategorized
             
         @endif</h5>
+
+        <div class="tags">
+            @if (!$post->tags->isEmpty())
+                @foreach ($post->tags as $tag)
+                    <span class="badge badge-success">
+                        {{$tag->name}}
+                    </span>
+                @endforeach
+            @else
+                <span class="badge badge-danger">No tags for this post</span>
+            @endif
+        </div>
         <p>
             {{$post->content}}
         </p>
