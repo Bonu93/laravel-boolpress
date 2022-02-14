f93e6982333d0318d6852ace69e48273efaf4b8e
Bonu93 <luca.morucci.kb@gmail.com>
2022-02-14 19:03:41 +0100

    add mail

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Mail\SendWelcomeEmail;
+use Illuminate\Support\Facades\Mail;
+
 
 class HomeController extends Controller
 {
     public function index() {
+        
+        Mail::to(Auth::user()->email)->send(New SendWelcomeEmail(Auth::user()->name));
+
+
         return view('admin.home');
     }
 }
---
+++ resources/views/mails/welcome.blade.php
@@ -0,0 +1,3 @@
+<h1>Hello, {{$name}}</h1>
+
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit at dolorem, explicabo dicta harum fuga qui porro dignissimos dolorum! Iusto officiis, vel dolores quo aliquid quibusdam molestias nostrum sunt deserunt.</p>
